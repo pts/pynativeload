4291e0e026e3b26f4fec4b77c0cb8c3614d83de3
Peter Szabo <pts@fazekas.hu>
2019-12-14 21:59:26 +0100

    updated nativeload.py

--- nativeload.py
+++ nativeload.py
@@ -1,8 +1,10 @@
 #! /usr/bin/python
 # by pts@fazekas.hu at Sat Dec 14 14:07:10 CET 2019
-# !! Make this work in Python 3.
+# !! Make this work in Python 3 with ctypes.
 
+import itertools
 import struct
+import sys
 
 
 def addmul(a, b, c, d, e, f, g, h, i):
@@ -10,6 +12,8 @@ def addmul(a, b, c, d, e, f, g, h, i):
   return (r & 0x7fffffff) - (r & 0x80000000)  # Sign-extend.
 
 # Size divisible by 16, good for alignment.
+# !! Support 10 arguments.
+# i386 (32-bit) only code, `import dl' doesn't support 64-bit.
 _compar_code = (
     # compar:  # TODO(pts): Make it smaller.
     # int compar(struct args *a, struct args *b) {
@@ -61,7 +65,26 @@ _compar_code = (
 assert not len(_compar_code) % 15
 
 
-# !! Add win32 support etc.
+def get_mmap_constants(_cache=[]):
+  if _cache:
+    return _cache[-1]
+  try:
+    mmap = __import__('mmap')
+  except ImportError:
+    mmap = type(sys)('fake-mmap')
+    pla = sys.platform
+    if pla.startswith('linux'):
+      mmap.PROT_READ, mmap.PROT_WRITE, mmap.PROT_EXEC = 1, 2, 4
+      mmap.MAP_PRIVATE, mmap.MAP_ANON = 2, 0x20
+    elif pla.startswith('darwin') or 'bsd' in pla:  # darwin is macOS.
+      mmap.PROT_READ, mmap.PROT_WRITE, mmap.PROT_EXEC = 1, 2, 4
+      mmap.MAP_PRIVATE, mmap.MAP_ANON = 2, 0x1000
+  missing_names = [name for name in ('PROT_READ', 'PROT_WRITE', 'PROT_EXEC', 'MAP_PRIVATE', 'MAP_ANON')
+                   if name not in mmap.__dict__]
+  if missing_names:
+    raise ImportError('Missing names in mmap module: %r' % missing_names)
+  _cache.append(mmap)
+  return mmap
 
 
 class CallerDl(object):
@@ -75,28 +98,33 @@ class CallerDl(object):
       raise TypeError
     if not isinstance(addr_map, dict):
       raise TypeError
-    import dl # 32-bit, Unix, Python 2 only.  !! Verify.
-    # !! Linux. Is macOS different?
-    import mmap  # For constants only.
+    dl = __import__('dl') # 32-bit, Unix, Python 2 only.  !! Verify.
+    mmap = get_mmap_constants()
     d = dl.open('')
-    assert d.sym('mmap')  # !! No assert please.
-    assert d.sym('mprotect')
-    assert d.sym('munmap')
-    assert d.sym('memcpy')
-    assert d.sym('qsort')
-    if d.sym('callr9'):
+    missing_names = []
+    try:
+      # Regular `import dl' doesn't support int as the 1st argument of
+      # d.call, but the one in StaticPython does support it, and we can use it
+      # for speedup.
+      d.call(d.sym('memcpy'))  # 0 is the default for the remaining args.
       compar_ofs = -1
-    else:
+    except TypeError:
+      if not d.sym('qsort'):
+        missing_names.append('qsort')
       compar_ofs = (len(native_code) + 15) & ~15
-      native_code += '\x90' * (compar_ofs - len(native_code)) + _compar_code  # !! Omit _compar_code if not needed.
-    self.d_call, self.size = vp, d.call, len(native_code)
-    self.vp = vp = d.call('mmap', 0, len(native_code),
-                 mmap.PROT_READ | mmap.PROT_WRITE,
-                 mmap.MAP_PRIVATE | mmap.MAP_ANON, -1, 0)
-    assert vp != 0
-    assert vp != -1
+      native_code += '\x90' * (compar_ofs - len(native_code)) + _compar_code
+    missing_names.extend(name for name in ('mmap', 'mprotect', 'munmap', 'memcpy') if not d.sym(name))
+    if missing_names:
+      raise RuntimeError('CallerDl unusable, names %r missing.' % missing_names)
+    self.d_call, self.size = d.call, len(native_code)
+    self.vp = vp = d.call(
+        'mmap', -1, len(native_code), mmap.PROT_READ | mmap.PROT_WRITE,
+        mmap.MAP_PRIVATE | mmap.MAP_ANON, -1, 0)
+    if vp in (0, -1):
+      raise RuntimeError('mmap failed.')
     d.call('memcpy', vp, native_code, len(native_code))
-    d.call('mprotect', vp, len(native_code), mmap.PROT_READ | mmap.PROT_EXEC)
+    if d.call('mprotect', vp, len(native_code), mmap.PROT_READ | mmap.PROT_EXEC):
+      raise RuntimeError('mprotect failed.')
     if compar_ofs >= 0:
       self.vp_compar = vp + compar_ofs
     else:
@@ -104,7 +132,7 @@ class CallerDl(object):
     self.vp_addr_map = dict((k, vp + v) for k, v in addr_map.iteritems())
 
   def __del__(self):
-    if self.vp and self.vp != -1:
+    if self.vp not in (0, 1):
       self.d_call('munmap', self.vp, self.size)
       self.vp = 0
     # We can't call dlclose(3) directly, `import dl' doesn't have that.
@@ -120,27 +148,18 @@ class CallerDl(object):
       a = [0] * 22
       for i, arg in enumerate(args):
         if isinstance(arg, str):
-          arg = d_call('memcpy', arg, 0, 0)  # Convert data pointer to integer.
+          arg = d_call('memcpy', arg)  # Convert data pointer to integer.
         a[i + 1] = arg
       a[0], a[10], a[11] = 3, self.vp_addr_map[func_name], 4
       qsort_data = struct.pack('=22l', *a)
       d_call('qsort', qsort_data, 2, 44, self.vp_compar)
       return struct.unpack('=l', qsort_data[4 : 8])[0]
     else:  # Fast version with callr9, StaticPython-only.
-      # !! Make it faster by using calll9, no need for a.extend.
-      a = ['callr9']
-      a.extend(args)
-      padc = 10 - len(a)
-      if padc:
-        if padc < 0:
-          raise ValueError('At most 9 arguments accepted.')
-        a.extend((0, 0, 0, 0, 0, 0, 0, 0, 0, 0)[:padc])
-      a.append(self.vp_addr_map[func_name])
-      return self.d_call(*a)
+      return self.d_call(self.vp_addr_map[func_name], *args)
 
 
 class CallerCtypes(object):
-  """Uses `import dl'. Works in Python 2 on i386 Unix (not Windows) only."""
+  """Uses `import ctypes'. Works in Python >=2.5."""
 
   __slots__ = ('vp', 'size', 'munmap', 'vp_addr_map', 'vp_func_map', 'c_char_p')
 
@@ -150,13 +169,18 @@ class CallerCtypes(object):
       raise TypeError
     if not isinstance(addr_map, dict):
       raise TypeError
-    import ctypes
-    import mmap  # !! Just constants.
+    ctypes = __import__('ctypes')
+    # !! Add win32 support for ctypes, no mmap and especially 64-bit (different ABI).
+    mmap = get_mmap_constants()
     self.munmap, self.size = ctypes.pythonapi['munmap'], len(native_code)
     self.munmap.argtypes = (ctypes.c_size_t, ctypes.c_size_t)
     mmap_func = ctypes.pythonapi['mmap']
     mmap_func.restype = ctypes.c_long
-    assert ctypes.pythonapi['mmap'].argtypes is None  # Doesn't change the original.
+    if ctypes.pythonapi['mmap'].argtypes is not None:
+      # Each time we use ctypes.pythonapi[...], we want to have a fresh
+      # ctypes._FuncPtr object, to avoid making global changes.
+      raise RuntimeError('Unusual global argtypes behavior detected.')
+      # Doesn't change the original.
     self.vp = vp = mmap_func(
         -1, len(native_code), mmap.PROT_READ | mmap.PROT_WRITE,
         mmap.MAP_PRIVATE | mmap.MAP_ANON, -1, 0)  # !! mprotect.
@@ -179,7 +203,7 @@ class CallerCtypes(object):
       self.vp = 0
 
   def callc(self, func_name, *args):
-    return self.vp_func_map[func_name](*map(self.c_char_p, args))
+    return self.vp_func_map[func_name](*itertools.imap(self.c_char_p, args))
 
 
 if __name__ == '__main__':
@@ -239,7 +263,7 @@ if __name__ == '__main__':
   native_code = addmul_code + xorp32_code + xorp32_amd64_code + addmul_amd64_code
   addr_map = {'addmul': 0, 'xorp32': len(addmul_code), 'xorp32_amd64': len(addmul_code) + len(xorp32_code), 'addmul_amd64': len(addmul_code) + len(xorp32_code) + len(xorp32_amd64_code)}
 
-  if 0:
+  if 1:
     caller = CallerDl(native_code, addr_map)
     print caller.callc('addmul', 5, 6, 7, 8, 9, 10, 11, 12, 13)
     sa = 'ABCD' + chr(0)  # !! Create unique string object faster: str(buffer(...))?
@@ -247,9 +271,10 @@ if __name__ == '__main__':
     caller.callc('xorp32', sa, sb)
     print [sa, sb]  #: ['%!!%\x00', 'dcba'].
 
-  caller = CallerCtypes(native_code, addr_map)
-  print caller.callc('addmul_amd64', 5, 6, 7, 8, 9, 10, 11, 12, 13)
-  sa = 'ABCD' + chr(0)  # !! Create unique string object faster: str(buffer(...))?
-  sb = 'dcba'
-  caller.callc('xorp32_amd64', sa, sb)
-  print [sa, sb]  #: ['%!!%\x00', 'dcba'].
+  if 0:
+    caller = CallerCtypes(native_code, addr_map)
+    print caller.callc('addmul_amd64', 5, 6, 7, 8, 9, 10, 11, 12, 13)
+    sa = 'ABCD' + chr(0)  # !! Create unique string object faster: str(buffer(...))?
+    sb = 'dcba'
+    caller.callc('xorp32_amd64', sa, sb)
+    print [sa, sb]  #: ['%!!%\x00', 'dcba'].
